ac6e49b50452d6ffacd9b13d6093398d0e9ea32b
Orozbekov <143519360+Imd19851107@users.noreply.github.com>
2023-09-09 12:15:09 +0600

    First Commit
    First Commit

---
+++ 1111.dart
@@ -0,0 +1,73 @@
+void main() {
+  // Задача 1
+  int a1 = 0;
+  if(a1 == 10) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 2
+  int a2 = 0;
+  if(a2 == 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 3
+  int a3 = 1;
+  if(a3 > 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 4
+  int a4 = 1;
+  if(a4 < 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 5
+  int a5 = 0;
+  if(a5 >= 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 6
+  int a6 = 1;
+  if(a6 <= 0) {
+    print('Верно');
+  } else {
+    return print('Неверно');
+  }
+
+  // Задача 7
+  int a7 = 0;
+  if(a7 != 0) {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 8
+  String a8 = 'test';
+  if(a8 == 'test') {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+
+  // Задача 9
+  String a9 = '1';
+  if(a9 == '1') {
+    print('Верно');
+  } else {
+    print('Неверно');
+  }
+}
